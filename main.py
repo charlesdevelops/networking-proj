6dc7fe1cb9576955ebeb14a4c60658de23eea2eb
charles <charlesaidanwatson@icloud.com>
2024-05-04 15:07:08 +0800

    add udp support in addition to tcp

--- main.py
+++ main.py
@@ -1,23 +1,60 @@
 import socket
 
-HOST = ''  # Listen on all available interfaces
-PORT = 2401  # Port number to listen on (from the command-line arguments)
+# TCP/IP socket for web browser communication
+HOST_TCP = ''
+PORT_TCP = 2401
+
+# UDP/IP socket for station-to-station communication
+HOST_UDP = ''
+PORT_UDP = 2408
+NEIGHBORS = [('host2', 2560), ('host3', 2566)]  # Neighbor station addresses
 
 # Create a TCP/IP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket_tcp.bind((HOST_TCP, PORT_TCP))
+server_socket_tcp.listen(5)
 
-# Allow reuse of the socket address
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# Create a UDP/IP socket
+server_socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket_udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket_udp.bind((HOST_UDP, PORT_UDP))
 
-# Bind the socket to the host and port
-server_socket.bind((HOST, PORT))
+print(f'Server listening on TCP {HOST_TCP}:{PORT_TCP} and UDP {HOST_UDP}:{PORT_UDP}')
 
-# Listen for incoming connections
-server_socket.listen(5)  # Queue up to 5 connection requests
+def handle_tcp_connection(client_socket_tcp, client_address_tcp):
+    try:
+        print(f'Connection from {client_address_tcp}')
 
-print(f'Server listening on {HOST}:{PORT}')
+        # Receive the HTTP request from the web browser
+        request = client_socket_tcp.recv(1024).decode()
+        print(f'Received request: {request}')
 
-def response_html():
+        # Generate the HTTP/1.1 response
+        response = create_http_response()
+
+        # Send the HTTP/1.1 response back to the web browser
+        client_socket_tcp.sendall(response)
+
+    finally:
+        # Clean up the connection
+        client_socket_tcp.close()
+
+def handle_udp_datagram(data, address):
+    print(f'Received UDP datagram from {address}: {data.decode()}')
+    # Process the received UDP datagram
+    # ...
+
+    # Send a response back to the neighbor station (if needed)
+    response = b'UDP response from this station'
+    server_socket_udp.sendto(response, address)
+
+    # Optionally, forward the datagram to other neighbor stations
+    for neighbor in NEIGHBORS:
+        if neighbor != address:
+            server_socket_udp.sendto(data, neighbor)
+
+def create_http_response():
     html_content = """
 <!DOCTYPE html>
 <html>
@@ -26,31 +63,19 @@ def response_html():
 </head>
 <body>
     <h1>Hello, World!</h1>
-    <p>Served via Python <3</p>
+    <p>Served from Python <3</p>
 </body>
 </html>
 """
+
     http_response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {len(html_content)}\r\n\r\n{html_content}"
-    return http_response
+    return http_response.encode()
 
 while True:
-    # Wait for a connection
-    print('Waiting for a connection...')
-    client_socket, client_address = server_socket.accept()
+    # Handle incoming TCP connections from web browsers
+    client_socket_tcp, client_address_tcp = server_socket_tcp.accept()
+    handle_tcp_connection(client_socket_tcp, client_address_tcp)
 
-    try:
-        print(f'Connection from {client_address}')
-
-        # Receive the HTTP request from the web browser
-        request = client_socket.recv(1024)
-        print(f'Received request: {request.decode()}')
-
-        # Generate the HTML response
-        html_response = response_html()
-
-        # Send the HTML response back to the web browser
-        client_socket.sendall(html_response.encode())
-
-    finally:
-        # Clean up the connection
-        client_socket.close()
+    # Handle incoming UDP datagrams from other station servers
+    data, address = server_socket_udp.recvfrom(1024)
+    handle_udp_datagram(data, address)
