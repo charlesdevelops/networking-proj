cd0790c8fcbb8a36a90a62fb2c6831ccfeb8d815
HarryKomah <harrykomah2003@gmail.com>
2024-05-19 22:37:57 +0800

    transfer files to ec2

--- files/startstations.sh
+++ files/startstations.sh
@@ -1,5 +1,20 @@
-./station-server TerminalA 4001 4002 172.24.118.122:4006 172.24.118.122:4008 172.24.118.122:4010 &
-./station-server JunctionB 4003 4004 172.24.118.122:4006 &
-./station-server BusportC 4005 4006 172.24.118.122:4002 172.24.118.122:4004 172.24.118.122:4010 &
-./station-server StationD 4007 4008 172.24.118.122:4002 &
-./station-server TerminalE 4009 4010 172.24.118.122:4002 172.24.118.122:4006 &
+./station-server StationA 4001 4002 localhost:4004 localhost:4024 &
+./station-server TerminalB 4003 4004 localhost:4002 localhost:4018 localhost:4024 localhost:4030 &
+./station-server JunctionC 4005 4006 localhost:4012 localhost:4036 &
+./station-server BusportD 4007 4008 localhost:4014 localhost:4034 localhost:4038 &
+./station-server StationE 4009 4010 localhost:4028 &
+./station-server TerminalF 4011 4012 localhost:4006 localhost:4014 localhost:4018 localhost:4022 localhost:4032 &
+./station-server JunctionG 4013 4014 localhost:4008 localhost:4012 &
+./station-server BusportH 4015 4016 localhost:4038 &
+./station-server StationI 4017 4018 localhost:4004 localhost:4012 localhost:4034 &
+./station-server TerminalJ 4019 4020 localhost:4032 localhost:4036 localhost:4040 &
+./station-server JunctionK 4021 4022 localhost:4012 localhost:4038 &
+./station-server BusportL 4023 4024 localhost:4002 localhost:4004 &
+./station-server StationM 4025 4026 localhost:4040 &
+./station-server TerminalN 4027 4028 localhost:4010 localhost:4030 localhost:4032 &
+./station-server JunctionO 4029 4030 localhost:4004 localhost:4028 localhost:4038 &
+./station-server BusportP 4031 4032 localhost:4012 localhost:4020 localhost:4028 &
+./station-server StationQ 4033 4034 localhost:4008 localhost:4018 localhost:4038 &
+./station-server TerminalR 4035 4036 localhost:4006 localhost:4020 &
+./station-server JunctionS 4037 4038 localhost:4008 localhost:4016 localhost:4022 localhost:4030 localhost:4034 &
+./station-server BusportT 4039 4040 localhost:4020 localhost:4026 &
